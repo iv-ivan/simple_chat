2f74f1a30b94ce720089843d791bc016cc5d3dbc
Ivan Ivashkovskiy <ivan290895@yandex.ru>
2020-04-27 21:35:24 +0300

    Initial Commit

---
+++ chat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chat
+//
+//  Created by Ivan Ivashkovskii on 27/04/2020.
+//  Copyright © 2020 Ivan Ivashkovskii. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
